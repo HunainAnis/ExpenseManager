2cb25baba5e13ac6d7cf2b85c2746ae90cb8c1e9
Muhammad Hunain <HunainAnis@users.noreply.github.com>
2020-06-21 15:46:54 +0500

    delete function updated!

--- src/components/allTransactions.js
+++ src/components/allTransactions.js
@@ -6,6 +6,10 @@ import { GlobalContext } from '../MainState'
 export const AllTransactions = () => {
     const { delTrans, transactions } = useContext(GlobalContext)
     console.log(transactions)
+    const controlDel = (id) => {
+        delTrans(id)
+        alert(`'Transaction of ${transactions.filter(i=>i.id===id)[0].name} is deleted successfully!'`)
+    }
     return(
         <Row>
             <Col>
@@ -16,7 +20,7 @@ export const AllTransactions = () => {
                             <Row>
                                 <Col xs={{ size:6, offset:1 }}><h6 style={{marginTop:'auto', marginBottom:'auto'}}>{trans.name}</h6></Col>
                                 <Col xs='3'><h6 style={{marginTop:'auto', marginBottom:'auto'}}>{trans.type === 'expense'?'-':'+'}${Math.abs(trans.amount)}</h6></Col>
-                                <Col><Button onClick={() => delTrans(trans.id)} color='white' style={{color:'red'}}>X</Button></Col>
+                                <Col><Button onClick={() => controlDel(trans.id)} color='white' style={{color:'red'}}>X</Button></Col>
                             </Row>
                         </Card>
                     ))
